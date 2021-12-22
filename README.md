80590682f0ce3fa6ba449daef015af2bf353ad0c
Marco Zatta <marco.zatta@microsoft.com>
2021-12-22 16:25:40 +0100

    Add how-to run instructions and some docstrings

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 This repository implements a simulation of the card game Blackjack that can
 be used with the Bonsai platform.
+
+## Run it locally
+
+In order to run the simulation locally you must install the dependencies in
+`requirements.txt` and then
+
+```bash
+python main.py
+```
+
+Please remember to set in your shell environment the variables
+`SIM_WORKSPACE` and `SIM_ACCESS_KEY`.
--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,4 +1,21 @@
-"""Simulation of Blackjack."""
+"""
+Simulation of Blackjack.
+
+This is a simplified version of blackjack with the following features:
+
+- Cards are picked from a french deck of infinite size, that is the
+  probability of picking a card is always the same at each iteration.
+- At the first hand the player is given two cards and the dealer one
+- If the player has 21, the game ends irrespective on any action that
+  the player takes
+- At each step the player can choose whether to ``stay`` or ``hit``
+- If the player chooses ``hit``, the dealer picks a card for the player
+- The player can choose ``hit`` until the sum of cards is higher than 21,
+  in that case the player loses
+- When the player ``stay`` the dealer picks a card for itself and the value of
+  player and dealer hands are counted
+- Who has a hand closer to 21 wins the game, if both have 21 the game is a draw
+"""
 import itertools
 import dataclasses
 import random
@@ -45,6 +62,15 @@ class Hand:
 
     @property
     def value(self) -> int:
+        """
+        Count the value of the hand.
+
+        Figures like J, Q, and K count as ten. Digits count by their
+        value and aces count like 1 or 11 depending on the best value.
+
+        For example, in case of 2 aces the value is 12, in case of 2
+        figures and 1 ace the value is 21.
+        """
         aces = 0
         cumulative = 0
         for card in self.cards:
