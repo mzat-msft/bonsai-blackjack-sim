e40d290c52be7d10ddda806fbdb4db2c5591f52e
Marco Zatta <marco.zatta@microsoft.com>
2022-01-13 08:38:29 +0100

    Reduce complexity of basic strategy

--- blackjack/policies.py
+++ blackjack/policies.py
@@ -44,7 +44,6 @@ class BasicPolicy(Policy):
 
     @staticmethod
     def strategy_matrix(player: Hand, dealer: Hand, player_ace: bool):
-        """Apply strategy from https://www.blackjackapprenticeship.com/blackjack-strategy-charts/."""  # noqa
         # Soft hand
         if player_ace and len(player) < 3:
             if player.has_rank(8) and dealer.is_ranks(6):
@@ -55,30 +54,30 @@ class BasicPolicy(Policy):
                 return 2
             elif player.has_rank(7) and dealer.has_rank_between(7, 8):
                 return 0
-            elif player.has_rank(7) and dealer.has_rank_between(9, 11):
-                return 1
-            elif player.has_rank(6) and dealer.is_ranks(2):
-                return 1
-            elif player.has_rank_between(2, 5) and dealer.has_rank_between(2, 3):
-                return 1
-            elif player.has_rank_between(2, 6) and dealer.has_rank_between(7, 11):
-                return 1
-            elif player.has_rank_between(2, 3) and dealer.is_ranks(4):
+            elif (
+                player.has_rank(7) and dealer.has_rank_between(9, 11) or
+                player.has_rank(6) and dealer.is_ranks(2) or
+                player.has_rank_between(2, 5) and dealer.has_rank_between(2, 3) or
+                player.has_rank_between(2, 6) and dealer.has_rank_between(7, 11) or
+                player.has_rank_between(2, 3) and dealer.is_ranks(4)
+            ):
                 return 1
             else:
                 return 2
         # Hard hand
-        if 12 <= player.value <= 16 and dealer.has_rank_between(7, 11):
-            return 1
-        elif player.value == 12 and dealer.has_rank_between(2, 3):
+        if (
+            12 <= player.value <= 16 and dealer.has_rank_between(7, 11) or
+            player.value == 12 and dealer.has_rank_between(2, 3)
+        ):
             return 1
         elif player.value >= 12:
             return 0
         elif player.value == 10 and dealer.has_rank_between(10, 11):
             return 1
-        elif 10 <= player.value <= 11:
-            return 2
-        elif player.value == 9 and dealer.has_rank_between(3, 6):
+        elif (
+            10 <= player.value <= 11 or
+            player.value == 9 and dealer.has_rank_between(3, 6)
+        ):
             return 2
         else:
             return 1
